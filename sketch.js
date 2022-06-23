04423c54c44bceeedfe19d3c367291b20ddda359
githubkishores <106915264+githubkishores@users.noreply.github.com>
2022-06-23 18:29:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,42 +1,76 @@
-var sea,ship;
-var seaImg,shipImg;
-
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+var coin;
 function preload(){
-  seaImg = loadImage("sea.png");
-//Choose the correct option by uncommenting the right line to load the animation for ship.
-  shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
-  //shipImg1 = loadAnimation("ship-1","ship-1","ship-2","ship-1");
-  //shipImg1 = loadAnimation("1.png","2.png","3.png","4.png");
-
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
 function setup(){
+  
   createCanvas(400,400);
-  background("blue");
-
-  // Moving background
-  sea=createSprite(400,200);
-  sea.addImage(seaImg);
-  sea.scale=0.3;
-
   
-  ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
-  ship.scale =0.25;
+// Moving background
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+
+//creating boy running
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
   
+
+leftBoundary=createSprite(0,0,100,800);
+
+// leftBoundary.invisible = false;
+// leftBoundary.visible = true;
+// leftBoundary.invisible = true;
+// leftBoundary.visible = false;
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
 }
 
 function draw() {
   background(0);
+  path.velocityY = 4;
   
-//Uncomment the correct line to make the background move to create a forward moving effect for the boat.
-  //sea.velocityY = -3;
-  //sea.x = -3;
-  //sea.velocityX = -3;
-  //sea.velocityX = 3;
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
   
 
+  //code to reset the background
+
+  /*if(path.y > 400 ){
+    path.y = height/2;
+  }*/
+
+  /*if(path.y > 400 ){
+   path.y = height/2;
+  }*/
+
+  if(path.y > 400 ){
+ path.y = height/2;}
+
+ /*if(path.y > 400 ){path.y = height/2;}*/
   
-    
   drawSprites();
+}
+
+function spawnCoins(){
+  if(frameCount %60===0){
+    coin=createSprite(200, 200, 40, 20);
+    coin.velocityY=3
+    coin.addImage(coin.png);
+    coin.scale=0.3
+    coin.y=Math.round(random(200,200))
+  }
 }
